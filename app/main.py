e4baf8735ee05e12636a8ae05e0c733a73fdc440
Emma Cann <zgb8ts@virginia.edu>
2024-03-28 20:38:32 +0000

    MySQL Demo Day1

--- app/main.py
+++ app/main.py
@@ -5,9 +5,19 @@ from typing import Optional
 from pydantic import BaseModel
 import json 
 import requests
+import os
+import MySQLdb
+from fastapi.staticfiles import StaticFiles
 # import boto3
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+DBHOST = os.environ.get('DBHOST')
+DBUSER = os.environ.get('DBUSER')
+DBPASS = os.environ.get('DBPASS')
+DB = "zgb8ts" 
 
 # The URL for this API has a /docs endpoint that lets you see and test
 # your various endpoints/methods.
